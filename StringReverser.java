72e87e9ba422f4cb8918a7f72664b08fb1b4e947
L676n <123578060+L676n@users.noreply.github.com>
2023-01-28 16:50:56 +0300

    Add files via upload

---
+++ StringReverser.java
@@ -0,0 +1,27 @@
+
+package assiginment5;
+
+import java.util.Scanner;
+
+public class StringReverser {
+    
+    
+    public static void main(String[] args) {
+        String str;
+        System.out.println("Enter your a word: ");
+        Scanner scanner = new Scanner(System.in);
+        str = scanner.nextLine();
+        scanner.close();
+        String reversed = reverseString(str);
+        System.out.println("The reversed string is: " + reversed);
+    }
+
+    public static String reverseString(String str){
+        if (str.isEmpty())
+            return str;
+        //Calling Function Recursively
+        return reverseString(str.substring(1)) + str.charAt(0);
+    }
+}
+    
+
